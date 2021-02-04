91c165d044ae8babdcb2433dd82f70c257cd021b
yemmp <yan.emmanuel@gmail.com>
2021-02-04 17:50:19 -0300

    Commit - 04/02

--- src/ponto-rota/ponto-rota.service.ts
+++ src/ponto-rota/ponto-rota.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
 import { CreatePontoRotaDto } from './dto/create-ponto-rota.dto';
 import { UpdatePontoRotaDto } from './dto/update-ponto-rota.dto';
+import { PontoRota } from './entities/ponto-rota.entity';
 
 @Injectable()
 export class PontoRotaService {
-  create(createPontoRotaDto: CreatePontoRotaDto) {
+  constructor(@InjectModel(PontoRota)private pontoRotaModel: typeof PontoRota){}
+  async create(createPontoRotaDto: CreatePontoRotaDto) {
     return 'This action adds a new pontoRota';
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all pontoRota`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} pontoRota`;
   }
 
-  update(id: number, updatePontoRotaDto: UpdatePontoRotaDto) {
+  async update(id: number, updatePontoRotaDto: UpdatePontoRotaDto) {
     return `This action updates a #${id} pontoRota`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} pontoRota`;
   }
 }
