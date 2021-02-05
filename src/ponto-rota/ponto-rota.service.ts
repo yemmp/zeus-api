4bb179df061f690546888b99e1030edc4a3421b1
yemmp <yan.emmanuel@gmail.com>
2021-02-05 10:28:33 -0300

    Commit - 05/02 pt 1

--- src/ponto-rota/ponto-rota.service.ts
+++ src/ponto-rota/ponto-rota.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { CreatePontoRotaDto } from './dto/create-ponto-rota.dto';
 import { UpdatePontoRotaDto } from './dto/update-ponto-rota.dto';
@@ -8,22 +8,53 @@ import { PontoRota } from './entities/ponto-rota.entity';
 export class PontoRotaService {
   constructor(@InjectModel(PontoRota)private pontoRotaModel: typeof PontoRota){}
   async create(createPontoRotaDto: CreatePontoRotaDto) {
-    return 'This action adds a new pontoRota';
+    try {
+      this.pontoRotaModel.create(createPontoRotaDto);
+    } catch (error) {
+      console.error('Erro ao Criar Ponto-Rota',error.message);
+      throw new BadRequestException();
+    }
+    return 'Ponto-Rota Criado com Sucesso';
   }
 
   async findAll() {
-    return `This action returns all pontoRota`;
+    try {
+      return this.pontoRotaModel.findAll();
+      
+    } catch (error) {
+      console.error('Erro ao Buscar Pontos-Rota',error.message);
+      throw new BadRequestException();
+    }
   }
 
   async findOne(id: number) {
-    return `This action returns a #${id} pontoRota`;
+    try {
+      return this.pontoRotaModel.findOne({where:{codPontoRota: id}});
+      
+    } catch (error) {
+      console.error(`Erro ao Buscar Ponto-Rota #${id}`,error.message)
+      throw new BadRequestException();
+    }
   }
 
   async update(id: number, updatePontoRotaDto: UpdatePontoRotaDto) {
-    return `This action updates a #${id} pontoRota`;
+    try {
+      PontoRota.update(updatePontoRotaDto,{where:{codPontoRota: id}}).then(()=>{
+        console.log(`Ponto-Rota #${id} Atualizado com Sucesso`);
+      })
+    } catch (error) {
+      console.error(`Erro ao Atualizar Ponto-Rota #${id}`,error.message);
+    }
+    return `Ponto-Rota #${id} Atualizado com Sucesso`;
   }
 
   async remove(id: number) {
-    return `This action removes a #${id} pontoRota`;
+    try {
+      this.pontoRotaModel.destroy({where:{codPontoRota:id}});
+    } catch (error) {
+      console.error(`Erro ao Deletar Ponto-Rota #${id}`,error.message);
+      throw new BadRequestException();
+    }
+    return `Ponto-Rota #${id} Deletado!`;
   }
 }
