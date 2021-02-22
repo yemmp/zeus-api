9f04a7c105156f9194de94dd8f6c710b0e78e8fe
yemmp <yan.emmanuel@gmail.com>
2021-02-22 14:10:44 -0300

    Commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,10 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
-import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
-import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService, 
-    private authService:AuthService) {}
+    ) {}
 /*
   @Get()
   getHello(): string {
@@ -16,16 +12,5 @@ export class AppController {
   }
   */
 
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-
-  async login(@Request() request){
-    return this.authService.login(request.usuario);
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('perfil')
-  getPerfil(@Request() request){
-    return request.usuario;
-  }
+ 
 }
